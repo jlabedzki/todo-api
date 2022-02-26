c60a85ca5226b061583b8f78f0689f28161ff3f0
Johnny <johnnylabedzki@hotmail.com>
2022-02-26 10:57:38 -0800

    Install flask-bcrypt, remove email field from users table, setup user creation and filter for existing users in register route

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, jsonify
+from flask import Flask, request, Response, jsonify
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_user, current_user, logout_user
 from db.models import *
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 # Configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://johnny:johnny@localhost:5432/todo-api'
@@ -13,9 +15,40 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.init_app(app)
 
+#
 
-@app.route('/', methods=['GET'])
-def index():
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json['username']
+    password = request.json['password']
+
+    user_exists = db.session.query(db.exists().where(
+        User.username == username)).scalar()
+
+    if user_exists:
+        return jsonify({"hello": "world"})
+        return Response(409)
+    else:
+        # hash password before storing in db
+        hashed_password = bcrypt.generate_password_hash(password)
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"user": "created"})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    # if account exists and credentials validate, return 200 and the userID
+    # if credentials fail validation return 401 (invalid password)
+    # if account doesn't exist return 404 (we could not find an account associated with that username)
     return jsonify({"hello": "world"})
 
 
--- src/db/models.py
+++ src/db/models.py
@@ -9,7 +9,4 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
-
-    db.create_all()
