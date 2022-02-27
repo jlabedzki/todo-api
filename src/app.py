7e04935027ee93df7f710662df3b5b5c528e2ab7
Johnny <johnnylabedzki@hotmail.com>
2022-02-26 16:02:05 -0800

    setup login logic, install Flask-Migrate

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response, jsonify
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_user, current_user, logout_user
 from db.models import *
@@ -6,16 +7,16 @@ from db.models import *
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
+
 # Configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://johnny:johnny@localhost:5432/todo-api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Initilialize login manager
 login = LoginManager(app)
 login.init_app(app)
-
-#
 
 
 @login.user_loader
@@ -32,24 +33,39 @@ def register():
         User.username == username)).scalar()
 
     if user_exists:
-        return jsonify({"hello": "world"})
-        return Response(409)
+        return Response(status=409)
     else:
         # hash password before storing in db
         hashed_password = bcrypt.generate_password_hash(password)
         user = User(username=username, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-
-        return jsonify({"user": "created"})
+        return Response(status=201)
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    # if account exists and credentials validate, return 200 and the userID
-    # if credentials fail validation return 401 (invalid password)
-    # if account doesn't exist return 404 (we could not find an account associated with that username)
-    return jsonify({"hello": "world"})
+    username = request.json['username']
+    password = request.json['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        # Compare the password input to the hashed password in the db
+        if bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return jsonify({'user_id': user.id}), Response(status=200)
+        else:
+            return Response(status=401)
+    else:
+        return Response(status=404)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+
+    logout_user()
+    return Response(status=200)
 
 
 if __name__ == "__main__":
